8d55742a51f166fe232cd0ccbd82c99bead17630
Andrea Belin <andrea.belin@a5s104pc42.fib.upc.es>
2025-05-05 11:25:42 +0200

    Models added, works to run

---
+++ build/CMakeFiles/SRGGE.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home2/users/alumnes/1329957/Desktop/SRGGE_Base/gl3w/gl3w.c" "CMakeFiles/SRGGE.dir/gl3w/gl3w.c.o" "gcc" "CMakeFiles/SRGGE.dir/gl3w/gl3w.c.o.d"
+  "/home2/users/alumnes/1329957/Desktop/SRGGE_Base/Application.cpp" "CMakeFiles/SRGGE.dir/Application.cpp.o" "gcc" "CMakeFiles/SRGGE.dir/Application.cpp.o.d"
+  "/home2/users/alumnes/1329957/Desktop/SRGGE_Base/PLYReader.cpp" "CMakeFiles/SRGGE.dir/PLYReader.cpp.o" "gcc" "CMakeFiles/SRGGE.dir/PLYReader.cpp.o.d"
+  "/home2/users/alumnes/1329957/Desktop/SRGGE_Base/Scene.cpp" "CMakeFiles/SRGGE.dir/Scene.cpp.o" "gcc" "CMakeFiles/SRGGE.dir/Scene.cpp.o.d"
+  "/home2/users/alumnes/1329957/Desktop/SRGGE_Base/Shader.cpp" "CMakeFiles/SRGGE.dir/Shader.cpp.o" "gcc" "CMakeFiles/SRGGE.dir/Shader.cpp.o.d"
+  "/home2/users/alumnes/1329957/Desktop/SRGGE_Base/ShaderProgram.cpp" "CMakeFiles/SRGGE.dir/ShaderProgram.cpp.o" "gcc" "CMakeFiles/SRGGE.dir/ShaderProgram.cpp.o.d"
+  "/home2/users/alumnes/1329957/Desktop/SRGGE_Base/TriangleMesh.cpp" "CMakeFiles/SRGGE.dir/TriangleMesh.cpp.o" "gcc" "CMakeFiles/SRGGE.dir/TriangleMesh.cpp.o.d"
+  "/home2/users/alumnes/1329957/Desktop/SRGGE_Base/TriangleMeshInstance.cpp" "CMakeFiles/SRGGE.dir/TriangleMeshInstance.cpp.o" "gcc" "CMakeFiles/SRGGE.dir/TriangleMeshInstance.cpp.o.d"
+  "/home2/users/alumnes/1329957/Desktop/SRGGE_Base/VectorCamera.cpp" "CMakeFiles/SRGGE.dir/VectorCamera.cpp.o" "gcc" "CMakeFiles/SRGGE.dir/VectorCamera.cpp.o.d"
+  "/home2/users/alumnes/1329957/Desktop/SRGGE_Base/main.cpp" "CMakeFiles/SRGGE.dir/main.cpp.o" "gcc" "CMakeFiles/SRGGE.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
